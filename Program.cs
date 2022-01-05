f3eec11cecd1c85b0960f5ddd30606993fffc30b
ErElectrical <94623283+ErElectrical@users.noreply.github.com>
2022-01-05 05:00:19 +0000

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -10,6 +10,35 @@ namespace AdditionOfTwoNumber
 
         }
 
+        //MSTest is Unit Testing framework for writing Unit test cases which
+        // was developed by Microsoft.
+
+        //Unit testing breaks the program down into the smallest bit of code,
+        //usually function-level, and ensures that the function returns the
+        // value one expects.By using a unit testing framework, the unit tests
+        //become a separate entity which can then run automated tests on
+        //the program as it is being built.
+
+        //Unit Testing is based on the Arrange, Act, Assert methodology. You
+         //arrange your initial considerations, act on those conditions with
+          //your method under test, then assert something happened.
+
+        //The entire goal for unit testing is to find failure atmocity.
+        // which means that we have to find that case where our method fails
+
+        // we dont suppose to connect with external data source.
+        // it is because that our unit testing may effect that database on which other
+        // developer are also working on that source.
+        // it may effect there code.
+
+        // Benefit of unit Testing 
+
+        // safer refactoring
+        // smaller,tighten, and decoupled code.
+        // documentation of requirements.
+        // continous integration.
+        // valuee of tests increases over time
+
         public int  add(int num1,int num2)
         {
             int c = num1 + num2;
